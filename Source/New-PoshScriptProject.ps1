1af430d34623ed5a1d87ffd43cca286e4841b4c7
smartph <phuc.nguyen@smartpesa.com>
2021-05-24 23:35:11 +0700

    added pipeline

---
+++ Source/New-PoshScriptProject.ps1
@@ -0,0 +1,43 @@
+
+<#PSScriptInfo
+
+.VERSION 0.1
+
+.GUID f559dd26-fa6d-495d-ad75-6b9fd527bee9
+
+.AUTHOR phuc.vinh@outlook.com
+
+.COMPANYNAME PhucNguyen
+
+.COPYRIGHT
+
+.TAGS
+
+.LICENSEURI https://raw.githubusercontent.com/dumpvn/New-PoshScriptProject/main/LICENSE
+
+.PROJECTURI https://github.com/dumpvn/New-PoshScriptProject
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+
+
+.PRIVATEDATA
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Powershell Script Project 
+
+#> 
+Param()
+
+'initial version, not very helpful.'
